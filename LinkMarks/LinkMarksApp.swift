ca05418f63ad3b66f56bd1dd09f3126813e45777
Tenorshare <Tenorshare@Tenorshare.cn>
2020-11-30 17:17:52 +0800

    Initial Commit

---
+++ LinkMarks/LinkMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkMarksApp.swift
+//  LinkMarks
+//
+//  Created by dong wu on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct LinkMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
